65dfbe973dc47a4a0bdacf07396288d88d9526d4
Pavel Loginov <p.loginov@space307.com>
2020-06-06 15:37:06 +0300

    first exercises

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,28 @@
-# pcost.py
-#
-# Exercise 1.27
+import csv
+import sys
+
+
+def portfolio_cost(filename: str) -> float:
+    """Returns the total cost of the portfolio"""
+    
+    with open('Data/portfolio.csv', 'rt') as f:
+        total_cost = 0
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            try:
+                total_cost += int(row[1]) * float(row[2])
+            except ValueError:
+                print('broken line', row)
+
+    return total_cost
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+
+cost = portfolio_cost(filename)
+print(f'Total cost: {cost}')
